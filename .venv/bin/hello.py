803ee1a4d3478bb5011dd9f0fe4e179f19444434
ShinyaIshizaki <shinya.ishizaki.0311@gmail.com>
2024-02-08 20:36:46 +0900

    added some routes

--- .venv/bin/hello.py
+++ .venv/bin/hello.py
@@ -11,3 +11,23 @@ def index():
 @app.route("/hello")
 def hello():
   return "Hello, world!"
+
+@app.route("/user/<username>")
+def user_profile(username):
+  return f"User {escape(username)}"
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+  return f"Post {escape(post_id)}"
+
+@app.route("/path/<path:sub_path>")
+def show_subpath(sub_path):
+  return f"Subpath {escape(sub_path)}"
+
+@app.route("/projects/")
+def projects():
+  return "The project page"
+
+@app.route("/about")
+def about():
+  return "The about page"
